3afc141c8d985e38438fac053b65b24688882c8f
tanajp <50686226+tanajp@users.noreply.github.com>
2020-03-27 13:39:02 +0900

    Update app.py

--- res/app.py
+++ res/app.py
@@ -1,4 +1,3 @@
-import os
 import requests
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
@@ -14,15 +13,17 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+# インスタンスの生成
 app = Flask(__name__)
 
-line_channel_secret = "*******************"
-line_channel_access_token = "**********************"
+# 自身のチャネルシークレット, チャネルアクセストークン
+line_channel_secret = "ed19bc34fc7efe7eb6fa901d2b1dcc2a"
+line_channel_access_token = "rlatt0fHvQH0CC/dbisrylWc9jbNcHO1If0ZxQ8um0PlaGXLC3uhgtAuqjJ/iuDsElfJDsr1URwa625xPKwedJdzr0zhakLKd0LhE7Lu1WCfzvr+gnVaKeIsqtuTp87K1N83Qn1dnUF41K9ut5z49gdB04t89/1O/w1cDnyilFU="
 
 line_bot_api = LineBotApi(line_channel_access_token)
 handler = WebhookHandler(line_channel_secret)
 
-
+# Webhookの操作, X-Line-Signatureリクエストヘッダーをテキスト形式で取得
 @app.route("/callback", methods=['POST'])
 def callback():
 
@@ -38,6 +39,7 @@ def callback():
 
     return 'OK'
 
+# Line上で返すテキストメッセージを定義する
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
 
@@ -46,22 +48,33 @@ def handle_message(event):
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
 
+# kNNでユーザーベースの協調フィルタリング
 def res_knn(text):
-    ratings = pd.read_csv("https://raw.githubusercontent.com/tanajp/restaurants/master/restaurant.csv")
+    ratings = pd.read_csv("https://raw.githubusercontent.com/tanajp/rec_restaurants/master/restaurant.csv")
     data = ratings.pivot_table(index = 'name', columns = 'user_id', values = 'ratings').fillna(0)
     knn = NearestNeighbors(n_neighbors=9, algorithm = "brute", metric = "cosine")
     model_knn = knn.fit(data)
-    arr_an = []
-    arr_am = []
+    
+    search_word = []
+    rec_res = []
     distance, indice = model_knn.kneighbors(data.iloc[data.index == text].values.reshape(1, -1),n_neighbors=11)
     for i in range(0, 4):
         if  i == 0:
-            arr_an.append('{0}がお好きなのですね。\nあなたにお勧めのお店はこちらです。:   '.format(data[data.index == text].index[0]))
+            search_word.append('{0}がお好きなのですね。\nあなたにお勧めのお店はこちらです。:   '.format(data[data.index == text].index[0]))
         else:
-            arr_am.append('No.{0}:{1}'.format(i,data.index[indice.flatten()[i]]))
-    canditate = str(arr_an[0]) + str(arr_am[0]) + "  /  " + str(arr_am[1]) + "  /  " + str(arr_am[2])
+            rec_res.append('No.{0}:{1}'.format(i,data.index[indice.flatten()[i]]))
+    
+    url_list = []
+    for j in range(1, 4):
+        url = "https://gourmet.goo.ne.jp/restaurant/result/?kw={:s}".format(data.index[indice.flatten()[j]])
+        url_list.append(url)
 
-    return canditate
+    canditate = []
+    for k in range(3):
+        canditate.append(rec_res[k] + " / "  + "URL: " + url_list[k])
+    
+    return str(search_word[0]) + str(canditate[0]) + "  /  " + str(canditate[1]) + "  /  " + str(canditate[2])
 
+# サーバーの立ち上げ
 if __name__ == "__main__":
     app.run()
